7022ed9bbf4d75b70b62af7376903ad4129b2436
hexatester <hexatester@protonmail.com>
2023-03-28 12:00:45 +0700

    Initial rosebackup

---
+++ rosebackup/__main__.py
@@ -0,0 +1,46 @@
+import sys
+from typing import Dict, List, Optional
+
+
+def make_fname(fname: str) -> str:
+    return fname.lstrip("/").replace(" ", "-").strip("\n")
+
+
+def save_dirf(fname: str, contents: List[str], headers: Optional[List[str]] = None):
+    filename = make_fname(fname)
+    with open(f"{filename}.rsc", "w") as writer:
+        if headers:
+            writer.writelines(headers)
+        writer.write(fname)
+        writer.writelines(contents)
+
+
+def main():
+    if len(sys.argv) < 2:
+        return print(f"Usage: {sys.argv[0]} <filename>")
+    filename = sys.argv[1]
+    with open(filename, "r") as reader:
+        contents = reader.readlines()
+        if not contents:
+            return print(f"{filename} empty!")
+    headers = list()
+    dirf = dict()
+    dirt = None
+    for content in contents:
+        if not dirt and content.startswith("#"):
+            headers.append(content)
+            continue
+        if content.startswith("/"):
+            dirt = content
+            continue
+        if not dirt:
+            continue
+        if dirt not in dirf:
+            dirf[dirt] = list()
+        dirf[dirt].append(content)
+    for k, v in dirf.items():
+        save_dirf(k, v, headers)
+
+
+if __name__ == "__main__":
+    main()
